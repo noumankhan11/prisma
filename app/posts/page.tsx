9b94422dd7e30e65752176ac41b90ab978b61b43
Nouman Khan <nomiofficial68@gmail.com>
2024-09-18 23:20:52 +0500

    queried database

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,20 @@
+import prisma from "@/lib/db";
+import { Prisma } from "@prisma/client";
+import Link from "next/link";
 import React from "react";
 
-export default function Posts() {
+export default async function Posts() {
+  const posts = await prisma.post.findMany();
   return (
-    <div>
-      <h1 className="font-xlg font-bold">ALL posts:</h1>
+    <div className="bg-gray-800 w-[600px] min-h-[30vh]">
+      {posts.map((post) => (
+        <Link
+          href={`/posts/${post.id}`}
+          key={post.id}
+          className="p-2 border-2 cursor-pointer">
+          {post.title}
+        </Link>
+      ))}
     </div>
   );
 }
